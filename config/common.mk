d3992b7d3b3c818eb26f6619ecc35fd7ed3ab64c
cristianvaz <cristianvaz81993@gmail.com>
2022-09-29 10:58:01 +0200

    config: Build GameSpace

--- config/common.mk
+++ config/common.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# Palyrim Packages
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Themes
 PRODUCT_PACKAGES += \
     LineageBlackTheme \
