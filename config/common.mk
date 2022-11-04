7eb7d4ec34f0cd8b02f227217f5e5dd9d96eb985
cristianvaz <cristianvaz81993@gmail.com>
2022-11-04 15:04:46 +0100

    vendor: Restore FaceUnlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354

--- config/common.mk
+++ config/common.mk
@@ -125,6 +125,17 @@ TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
 $(call inherit-product, vendor/lineage/bootanimation/bootanimation.mk)
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Lineage packages
 PRODUCT_PACKAGES += \
     Aperture \
