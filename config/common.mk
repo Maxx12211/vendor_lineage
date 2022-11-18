Spark-Rom/platform_vendor_spark

vendor: Add QuickTap sysconfig
Change-Id: Ib80bd3edec4b197603c4b9937428db2536d84e0a

config/common.mk
@@ -227,3 +227,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     hosts.spark_adblock
+
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/spark/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif
+

Maxx12211/vendor_lineage

config/common.mk
@@ -260,3 +260,9 @@ $(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
 -include vendor/addons/addons.mk
+
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif